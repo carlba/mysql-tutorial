9d50b65c2f2575893eabde620ee98291c7d68989
Carl Bäckström <genzorg@gmail.com>
2018-11-21 15:53:09 +0100

    Cleaned up exercise file

--- refining_selections/excercises.sql
+++ refining_selections/excercises.sql
@@ -7,7 +7,7 @@ SELECT title FROM books WHERE title LIKE '%stories%';
 SELECT title, pages FROM books ORDER BY pages DESC LIMIT 1;
 
 -- Print a summary containing the title and year, for the 3 most recent books
-select CONCAT(title,' - ', released_year) AS summary FROM books ORDER BY released_year DESC LIMIT 3;
+SELECT CONCAT(title,' - ', released_year) AS summary FROM books ORDER BY released_year DESC LIMIT 3;
 
 -- Find all books with an author_lname that contains a space(" ")
 SELECT title, author_lname FROM books WHERE author_lname LIKE '% %' ;
@@ -21,4 +21,5 @@ SELECT title, author_lname FROM books ORDER BY author_lname, title;
 
 -- Print title and author_lname, sorted first by author_lname and then by title
 -- Sorted Alphabetically By Last Name
-SELECT UPPER(CONCAT_WS(' ', 'my favorite author is', author_fname, author_lname, '!')) AS 'yell' from books ORDER BY author_lname;
+SELECT UPPER(CONCAT_WS(' ', 'my favorite author is', author_fname, author_lname, '!')) AS 'yell'
+FROM books ORDER BY author_lname;
